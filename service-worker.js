58e35ddbffd600e2e7bf5d3613d74f27b2360912
gabsdono <gabsdono@espol.edu.ec>
2026-08-22 21:58:47 -0500

    Resumen: etiqueta genérica 'Este mes' en vez del mes/año específico

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v4';
+const CACHE = 'gastos-v5';
 const ASSETS = ['./', './index.html', './logic.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
