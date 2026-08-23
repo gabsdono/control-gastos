b246e449b72d77bcfbc2a9bbfed24c0a8b1ea300
gabsdono <gabsdono@espol.edu.ec>
2026-08-22 21:42:46 -0500

    Tema claro: fondo blanco, colores vivos, paleta de categorías validada para fondo claro

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v3';
+const CACHE = 'gastos-v4';
 const ASSETS = ['./', './index.html', './logic.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
