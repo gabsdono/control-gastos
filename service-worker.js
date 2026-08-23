3461adf58778f215e7f6bb219dbaff1857d6daeb
gabsdono <gabsdono@espol.edu.ec>
2026-08-22 22:56:46 -0500

    Fix: no se podía elegir categoría/cuenta/frecuencia (botones quedaban dentro de un <label>, iOS redirigía el toque al primer botón)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v9';
+const CACHE = 'gastos-v10';
 const ASSETS = ['./', './index.html', './logic.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
