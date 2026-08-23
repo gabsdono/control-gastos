07c71c40e5a9482942606ea9d3c49fa0d3911a77
gabsdono <gabsdono@espol.edu.ec>
2026-08-22 23:39:57 -0500

    Fix: sin categorias, Resumen cortaba antes de dibujar Meses anteriores (el mes cerrado SI se guardaba, solo no se mostraba)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v14';
+const CACHE = 'gastos-v15';
 const ASSETS = ['./', './index.html', './logic.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
