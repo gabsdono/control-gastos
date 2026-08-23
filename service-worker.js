a3c8c1cc246f23604a8ea3b7dd16bd6406d27ea2
gabsdono <gabsdono@espol.edu.ec>
2026-08-22 23:24:10 -0500

    Meses anteriores: agregar botón para borrar un mes guardado por error

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v13';
+const CACHE = 'gastos-v14';
 const ASSETS = ['./', './index.html', './logic.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
