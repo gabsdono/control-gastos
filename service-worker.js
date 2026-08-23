1a5231fa39ff408082788c9f35ee2023fa68129e
gabsdono <gabsdono@espol.edu.ec>
2026-08-22 22:42:10 -0500

    Fix: la coma decimal no funcionaba en los montos (type=number la rechazaba)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v7';
+const CACHE = 'gastos-v8';
 const ASSETS = ['./', './index.html', './logic.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
