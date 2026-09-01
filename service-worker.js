2acf32012ccb6610a8fe2f0a80135aee4c66aad2
gabsdono <gabsdono@espol.edu.ec>
2026-09-01 08:54:26 -0500

    Mostrar el simbolo real de cada moneda (usando el locale nativo de cada una), no solo el codigo ISO

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v17';
+const CACHE = 'gastos-v18';
 const ASSETS = ['./', './index.html', './logic.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
