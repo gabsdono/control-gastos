9b5ba03b0c5124b43f875d5433935cb4021c0988
gabsdono <gabsdono@espol.edu.ec>
2026-09-01 08:47:54 -0500

    Agregar selector de moneda (ajustes en la barra superior) con las monedas mas relevantes por pais

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v15';
+const CACHE = 'gastos-v16';
 const ASSETS = ['./', './index.html', './logic.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
