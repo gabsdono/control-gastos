fb79a7bca89cef0cd131db775491114b4e5845f8
gabsdono <gabsdono@espol.edu.ec>
2026-09-01 23:07:24 -0500

    Poder editar movimientos (gastos/ingresos, fijos y variables) y los pagos/ahorros registrados en cada meta

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v18';
+const CACHE = 'gastos-v19';
 const ASSETS = ['./', './index.html', './logic.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
