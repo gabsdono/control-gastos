4151195f68e1128e74ed87db6090e591a72b4f7f
gabsdono <gabsdono@espol.edu.ec>
2026-09-01 23:10:44 -0500

    Movimientos fijos: botones subir/bajar para ordenarlos manualmente

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v19';
+const CACHE = 'gastos-v20';
 const ASSETS = ['./', './index.html', './logic.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
