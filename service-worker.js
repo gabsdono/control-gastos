4f14b0c55f26d66422cb767375e56eaf4cc1b018
gabsdono <gabsdono@espol.edu.ec>
2026-09-05 12:29:08 -0500

    Grafico de linea: mostrar el primer mes guardado aunque sea uno solo; refrescar la linea al borrar un mes del historial

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v24';
+const CACHE = 'gastos-v25';
 const ASSETS = ['./', './index.html', './logic.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
